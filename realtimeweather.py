e480cd2f3f1accf24a54dd012a74fbfd0e7fd2b6
Drashtant <147946273+Dr4shtant@users.noreply.github.com>
2024-07-05 13:54:33 +0530

    realtimeweather.py

--- realtimeweather.py
+++ realtimeweather.py
@@ -10,23 +10,31 @@ def get_weather(city):
     params = {
         'q': city,
         'appid': api_key,
-        'units': 'metric'  # Use imperial for Fahrenheit
+        'units': 'metric'  # Use 'imperial' for Fahrenheit
     }
-    response = requests.get(base_url, params=params)
+    response = requests.get(base_url, params = params)
     data = response.json()
     
     if response.status_code == 200:
         weather = data['weather'][0]['description']
         temperature = data['main']['temp']
+        feels_like = data['main']['feels_like']
         humidity = data['main']['humidity']
+        visibility = data['visibility']
         wind_speed = data['wind']['speed']
         latitude = data['coord']['lat']
         longitude = data['coord']['lon']
         pressure = data['main']['pressure']
-        
+        timezone_s = data['timezone']
+        timezone_min = (timezone_s%3600)//60
+        timezone_hr = timezone_s//3600
+
+        print(f"Timezone: {timezone_hr:02}:{timezone_min:02} UTC")
         print(f"Weather in {city}: {weather}")
         print(f"Temperature: {temperature}°C")
+        print(f"Feels like: {feels_like}°C")
         print(f"Humidity: {humidity}%")
+        print(f"Visibility: {visibility} m")
         print(f"Pressure: {pressure} hPa")
         print(f"Wind Speed: {wind_speed} m/s")
         print(f"Latitude: {latitude}")
